48ac1c5e55c7bc13b20bef4b7df36847ec9f2d19
JanKolo04 <jankolodziej04@gmail.com>
2021-09-27 13:45:36 +0200

    problem with adding button to one div

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-function myFunction() {
+function add_task() {
 	//get tasks div
 	var tasks_div = document.getElementById("tasks_all");
+
 	//create new div
-  	var new_div = document.createElement("DIV");
-  	new_div.className = "task";
-  	//get value from text input
-  	new_div.innerHTML = document.querySelector('#myText').value;
-  	//append text from input text to new div
-  	tasks_div.appendChild(new_div);
+  	var task = document.createElement("DIV");
+
+  	//class name
+  	task.id = "task";
+
+  	//append text from text input to task
+  	task.innerHTML = document.querySelector('#myText').value;
+
+  	//append task to all tasks
+  	tasks_div.appendChild(task);
   	
   	//clear text input
 	document.getElementById('myText').value = '';
+
+
+
+	//get task
+	var task = document.getElementById("task");
+	//create button
+	var button = document.createElement("BUTTON");
+	//add name into button
+	button.innerHTML = "Add";
+
+	//append button to task
+	task.appendChild(button);
+
+}
+
+
+
+
+
+
+
+function add_remove_button() {
+
+
+	var all_tasks = document.getElementById("tasks_all");
+
+	var task = document.getElementById("task");
+
+	var in_progress = document.getElementById("progress_task");
+
+
+
+	in_progress.appendChild(task);
+
+	all_tasks.removeChild(task);
 
 }
--- test/script.js
+++ test/script.js
@@ -1,5 +1,13 @@
-document.getElementById("newsectionbtn").onclick = function() {
-  var container = document.getElementById("container");
-  var section = document.getElementById("mainsection");
-  container.appendChild(section.cloneNode(true));
+function myFunction() {
+  //get tasks div
+  var tasks_div = document.getElementById("tasks");
+  //create new div
+    var new_div = document.createElement("DIV");
+    //get value from text input
+    new_div.innerHTML = document.querySelector('#myText').value;
+    //append text from input text to new div
+    tasks_div.appendChild(new_div);
+
+    //clear text input
+  document.getElementById('myText').value = '';
 }
