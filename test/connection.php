325d598d66fbfcaf9516d86421a534a57fd00696
JanKolo04 <jankolodziej04@gmail.com>
2021-10-18 19:10:07 +0200

    working login system

--- test/connection.php
+++ test/connection.php
@@ -1,11 +1,14 @@
-<?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "javatpoint";  
-      
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
-?> 
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db_name = "tasktable";
+
+    $mysqli = new mysqli($host, $user, $password, $db_name);
+
+    // Check connection
+    if ($mysqli -> connect_errno) {
+      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+      exit();
+    }
+?>
