9a9768eed9368bc0a80dffd7baa47409dabc0c01
JanKolo04 <jankolodziej04@gmail.com>
2021-10-31 20:30:16 +0100

    php register

---
+++ php/register.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+    include("connection.php");
+
+    //posted items
+    $login = $_POST['login'];
+    $email = $_POST['email'];
+    $password = $_POST['passwd'];
+    $rpassword = $_POST['rpassword'];
+
+    //adding to table
+    $sql = "INSERT INTO users (login, email, password) VALUES ('$login', '$email', '$password')";
+
+    //if connection is aproved things has been added
+    if ($con->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } 
+
+    //else print error
+    else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="style-register.css">
+</head>
+<body>
+
+    <div class="main">
+        <div class="white-background">
+
+
+        
+            <h1 id="text1">Register</h1>
+            <p id="text2">Enter login e-mail and password to create account</p>
+
+            <div class="allStuff">
+                <form method="post">
+
+                    <div class="login-email">
+                        <div class="Login">
+                            <center><input type="text" id="login" name="login" required placeholder="Login"></center>
+                        </div>
+
+                        <div class="Email">
+                            <center><input type="text" id="email" name="email" required placeholder="E-mail"></center>
+                        </div>
+                    </div>
+
+
+                    <div class="passwords">
+                        <div class="passwd1">
+                            <center><input type="password" id="passwd" name="passwd" required placeholder="Password"></center>
+                        </div>
+
+                        <div class="passwd2">
+                            <center><input type="password" id="rPasswd" name="rpasswd" required placeholder="Repeat Password"></center>
+                        </div>
+                    </div>
+
+
+                    <div class="submit-login">
+                        <div id="submit-div">
+                            <input type="submit" value="Submit" class="submit" id="submit">
+                        </div>
+
+                        <div id="login-div">
+                            <p id="loginText">If you have an account? <a id="loginLink" href="login.php">Login</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
