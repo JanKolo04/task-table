2486617240754a09f1c824e6c58736a954f6c3a7
JanKolo04 <jankolodziej99@gmail.com>
2021-12-14 21:21:59 +0100

    data from db in table

--- php_main/admin-panel.php
+++ php_main/admin-panel.php
@@ -16,6 +16,7 @@ if (session_status() == PHP_SESSION_ACTIVE) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin Panel</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
@@ -27,25 +28,49 @@ if (session_status() == PHP_SESSION_ACTIVE) {
 
 <?php
 
+//get keys form array
+
 if(array_key_exists('submit', $_POST)) {
 	printTable();
 }
 
 function printTable() {
 	global $con;
-	$komenda = $_POST['pole'];
 
+	$komenda = $_POST['pole'];
 	$query = mysqli_query($con, $komenda);
 
-	foreach ($query as $row) {
-		echo "<pre>";
-		print_r($row);
+	echo("<table border='1'>");
+	$first_row = true;
+
+	while ($row = mysqli_fetch_assoc($query)) {
+		if ($first_row) {
+			$first_row = false;
+
+
+			// Output header row from keys.
+			
+			echo '<tr>';
+			foreach ($row as $key => $field) {
+				echo '<th class="head">' . htmlspecialchars($key) . '</th>'; 
+			}
+
+			echo '</tr>';
+		}
+
+		echo '<tr>';
+		foreach($row as $key => $field) {
+			echo '<td class="id">' . htmlspecialchars($field) . '</td>';
+		}
+
+		echo '</tr>';
 	}
+	echo("</table>");
+
 }
-
-
 ?>
 
 
 </body>
 </html>
+
