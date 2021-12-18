4b6c36e65ac9ce63ff84bef6c2c716ec5e3b4935
JanKolo04 <jankolodziej04@gmail.com>
2021-12-18 22:43:29 +0100

    frontend for input password in forgot

---
+++ php_main/input-passwd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style-input-passwd.css">
+	<link rel="shortcut icon" href="images/t.png">
+	<title>Register password</title>
+</head>
+<body>
+
+	<div id="content">
+		<div id="textHolder">
+			<div id="textDiv1">
+				<h1 id="text1">Enter password</h1>
+			</div>
+
+			<div id="textDiv2">
+				<p id="text2">Create new password</p>
+			</div>
+		</div>
+
+		<div id="stuff">
+			<div id="inputsDiv">
+				<input class="input" required type="password" name="passwd1" placeholder="Password">
+				<input class="input" required type="password" name="passwd2" placeholder="Repeat password">
+			</div>
+
+			<div id="submitDiv">
+				<button id="button" type="submit" name="submit">Submit</button>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
