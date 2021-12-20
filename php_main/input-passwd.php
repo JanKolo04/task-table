f8b1fd185753758d4d7e83169a54831bddc03d1f
JanKolo04 <jankolodziej99@gmail.com>
2021-12-20 22:49:04 +0100

    php and js for input passwd

--- php_main/input-passwd.php
+++ php_main/input-passwd.php
@@ -1,3 +1,10 @@
+<?php 
+	session_start();
+
+	include("connection.php");
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,17 +28,55 @@
 		</div>
 
 		<div id="stuff">
-			<div id="inputsDiv">
-				<input class="input" required type="password" name="passwd1" placeholder="Password">
-				<input class="input" required type="password" name="passwd2" placeholder="Repeat password">
-			</div>
+			<form method="POST">
+				<div id="inputsDiv">
+					<input class="input" id="first" required type="password" name="first" placeholder="Password">
+					<input class="input" id="second" required type="password" name="second" placeholder="Repeat password">
+				</div>
 
-			<div id="submitDiv">
-				<button id="button" type="submit" name="submit">Submit</button>
-			</div>
+				<div id="submitDiv">
+					<button id="button" type="submit" name="submit" onclick="checking()">Submit</button>
+				</div>
+			</form>
 		</div>
 	</div>
 
 
+	<script type="text/javascript">
+		function checking() {
+			var first = document.getElementById("first").value;
+			var second = document.getElementById("second").value;
+
+			if(first != second) {
+				alert("password is diffrents!");
+			}
+		}
+	</script>
+
+
+	<?php
+
+		if(array_key_exists("submit", $_POST)) {
+			run();
+		}
+
+
+		function run() {
+			global $con;
+
+			$first = $_POST['first'];
+			$second = $_POST['second'];
+
+			//table name and data
+			$sql = "UPDATE test SET password='$first' WHERE login='janek'";
+			$query = mysqli_query($con, $sql);
+		}
+
+	?>
+
 </body>
 </html>
+
+
+
+
