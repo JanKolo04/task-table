a7df986abe7c2becfe272fac98ce96afdf7e0722
JanKolo04 <jankolodziej04@gmail.com>
2021-12-27 22:28:39 +0100

    create task from endTask without NULL variable

--- needed-files/create-task-with-data-from-db/main.php
+++ needed-files/create-task-with-data-from-db/main.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-include("connection.php");
+include("../connection.php");
 
 ?>
 
@@ -28,6 +28,7 @@ include("connection.php");
 
 <?php
 
+	
 	//get all elements from allTask column where login is LOGIN USER
 	$sqlAll = "SELECT allTask FROM test WHERE login='admin'";
 	$queryAll = mysqli_query($con, $sqlAll);
@@ -53,6 +54,16 @@ include("connection.php");
 	}
 
 
+	$sqlEnd = "SELECT endTask FROM test WHERE login='admin'";
+	$queryEnd = mysqli_query($con, $sqlEnd);
+
+	$arrayEnd;
+	while ($row = mysqli_fetch_row($queryEnd)) {
+		$convert = implode($row);
+		$arrayEnd[] = $convert;
+	}
+
+
 ?>
 
 <script type="text/javascript">	
@@ -62,11 +73,8 @@ include("connection.php");
 	    //get array length 
 	    var length = arrayAll.length;
 
-	    //div all
 	    var all = document.getElementById("all");
-	  	//for loop to get all elemetns from array
 		for (var i=0; i<length; ++i) {
-			//create task when have some text
 			if (arrayAll[i] != '') {
 
 			    //create div in for beacouse he create
@@ -125,10 +133,42 @@ include("connection.php");
 	}
 
 
-	//run functions
+	function end() {
+		//get array from php to js
+	    var arrayEnd = <?php echo json_encode($arrayEnd); ?>;
+	    //get array length 
+	    var length = arrayEnd.length;
+
+	    var end = document.getElementById("end");
+		for (var i=0; i<length; ++i) {
+			if (arrayEnd[i] != '') {
+
+			    //create div in for beacouse he create
+			    //as meany times as the for loop ca execute
+			    var task = document.createElement("DIV");
+			    task.className = "task";
+
+			    var p = document.createElement("p");
+			    p.className = "textP";
+
+			    //variable x is a element from array
+			    var x = arrayEnd[i];
+			    //append element to p
+			    p.innerHTML = x;
+
+			    //append task to end
+			    end.appendChild(task);
+			   	//append p to task
+			    task.appendChild(p);
+
+			}
+		}
+	}
+
 	window.onload = function() {
 		all();
 		pro();
+		end();
 	}
 
 
