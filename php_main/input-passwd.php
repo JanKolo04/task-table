b6946a02300240257dae6d857b552a9a530e484f
JanKolo04 <jankolodziej04@gmail.com>
2022-02-08 17:28:50 +0100

    new design for input passwd

--- php_main/input-passwd.php
+++ php_main/input-passwd.php
@@ -3,10 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/style-input-passwd.css">
-	<link rel="shortcut icon" href="images/t.png">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
-	<title>Register password</title>
 </head>
 <body>
 
@@ -21,72 +17,73 @@
 				</div>
 
 
-				<div id='content'>
+				<div id='InputDivMain'>
 					<div id='textHolder'>
-						<div id='textDiv1'>
-							<h1 id='text1'>Enter password</h1>
+						<div id='DivMainText'>
+							<h1 id='mainText'>Enter password</h1>
 						</div>
 
-						<div id='textDiv2'>
-							<p id='text2'>Create new password</p>
+						<div id='DivInfo'>
+							<p id='infoText'>Create new password</p>
 						</div>
 					</div>
 
-					<div id='stuff'>
-						<form method='POST'>
-							<div id='inputsDiv'>
-								<center>
-									<input class='input' id='first' required type='password' name='first' placeholder='Password'>
-									<i class='bi bi-eye-slash' id='togglePassword1'></i>
-								</center>
-
-
-								<center>
-									<input class='input' id='second' required type='password' name='second' placeholder='Repeat password'>
-									<i class='bi bi-eye-slash' id='togglePassword2'></i>
-								</center>
+					<form method='POST'>
+						<div id='inputsDiv'>
+							<div id='divInputFirst'>
+								<input class='input' id='first' required type='password' name='first' placeholder='Password'>
+								<i class='bi bi-eye-slash' id='togglePassword1'></i>
 							</div>
+						
+							<div id='divInputSecond'>
+								<input class='input' id='second' required type='password' name='second' placeholder='Repeat password'>
+								<i class='bi bi-eye-slash' id='togglePassword2'></i>
+							</div>	
+							
+						</div>
 
-							<script>
-								//input2
-								var button1 = document.getElementById('togglePassword1');
-								var input1 = document.getElementById('first');
+						<script>
+							//input2
+							var button1 = document.getElementById('togglePassword1');
+							var input1 = document.getElementById('first');
 
-								//input 2
-								var button2 = document.getElementById('togglePassword2');
-								var input2 = document.getElementById('second');
+							//input 2
+							var button2 = document.getElementById('togglePassword2');
+							var input2 = document.getElementById('second');
 
 
-								button1.onclick = function() {
-									if(input1.type == 'password') {
-										input1.setAttribute('type', 'text');
-										button1.className = 'bi bi-eye';
-									}
-
-									else {
-										input1.setAttribute('type', 'password');
-										button1.className = 'bi bi-eye-slash';
-									}
+							button1.onclick = function() {
+								if(input1.type == 'password') {
+									input1.setAttribute('type', 'text');
+									button1.className = 'bi bi-eye';
 								}
 
-								button2.onclick = function() {
-									if(input2.type == 'password') {
-										input2.setAttribute('type', 'text');
-										button2.className = 'bi bi-eye';
-									}
-
-									else {
-										input2.setAttribute('type', 'password');
-										button2.className = 'bi bi-eye-slash';
-									}
+								else {
+									input1.setAttribute('type', 'password');
+									button1.className = 'bi bi-eye-slash';
 								}
-							</script>
+							}
 
-							<div id='submitDiv'>
-								<button id='button' type='submit' name='submitInput'>Submit</button>
-							</div>
-						</form>
-					</div>
+							button2.onclick = function() {
+								if(input2.type == 'password') {
+									input2.setAttribute('type', 'text');
+									button2.className = 'bi bi-eye';
+								}
+
+								else {
+									input2.setAttribute('type', 'password');
+									button2.className = 'bi bi-eye-slash';
+								}
+							}
+						</script>
+
+						<div class='d-flex justify-content-end align-items-center' id='divButton'>					
+							<a href='login' id='cancelButton'>Cancel</a>
+					
+							<button id='submitButton' type='submit' name='submitInput'>Submit</button>
+						</div>
+
+					</form>
 				</div>";
 		}
 
